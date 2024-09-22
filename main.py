5abe3cbbe620799a0be0bf0b74ba44a9735262f9
marcos <m_german4@mail.fhsu.edu>
2024-09-22 16:00:09 -0500

    retrieve data and create bar chart

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Marcos German
 # Mini Project 2
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Load Netflix titles dataset
 df = pd.read_csv('titles.csv')
@@ -9,3 +10,14 @@ df = pd.read_csv('titles.csv')
 #Display info and preview dataset
 print(df.info())
 print(df.head())
+
+#Count number of movies released per year
+data = df['release_year'].value_counts().sort_index()
+
+#Create bar chart
+plt.figure(figsize=(10, 6))
+data.plot(kind='bar')
+plt.title('Number of Netflix Movies Released per Year')
+plt.xlabel('Year')
+plt.ylabel('Number of Titles')
+plt.show()
