0c72363bac5f2450e450a519457c0b28b560ecf8
marcos <m_german4@mail.fhsu.edu>
2024-09-22 15:55:36 -0500

    import dataset

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # INF601 - Advanced Programming in Python
 # Marcos German
 # Mini Project 2
+import pandas as pd
+
+#Load Netflix titles dataset
+df = pd.read_csv('titles.csv')
+
+#Display info and preview dataset
+print(df.info())
+print(df.head())
